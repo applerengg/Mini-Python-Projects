480f7f2b56c7d6452e0a9dba25dbb1b4f3bd9069
AlpEren Gençoğlu <alpgen99@hotmail.com>
2020-04-13 21:58:56 +0300

    snek v1.1.1

--- Snek/snek.py
+++ Snek/snek.py
@@ -1,8 +1,8 @@
 import random
-import time
+#import time
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"	# do not print message on pygame import
-import pygame 
+from pygame import *
 
 
 ###
@@ -11,14 +11,14 @@ import pygame
 # the main function. 
 ###
 class CFG():
-	CLR_SNEK = (32, 128, 0)
-	CLR_BG = (192, 192, 192)
-	CLR_FOOD = (255, 255, 32)
-	CLR_SCR_TXT = (32, 32, 192)
-	CLR_TTL_TXT = (192, 64, 64)
-	SIZE_SNEK = 12				# snake size in pixels
-	SIZE_WIN = (960, 540)		# Window size (width, height)
-	SPD = 30					# snake speed
+	CLR_SNEK = (32, 128, 0)			# Snek Color
+	CLR_BG = (220, 220, 220)		# Main game Background Color
+	CLR_FOOD = (255, 255, 32)		# Food color
+	CLR_SCR_TXT = (32, 32, 192)		# Score text color
+	CLR_TTL_TXT = (192, 64, 64)		# Title text color
+	SIZE_SNEK = 12					# snake size in pixels
+	SIZE_WIN = (960, 540)			# Window size (width, height)
+	SPD = 30						# snake speed
 
 	# Binary settings
 	BRDR_FOOD = True			# draw borders for food
@@ -28,9 +28,9 @@ class CFG():
 	ALWYS_SHW_TME = True 		# if set to true, shows time while playing, otherwise only when game ends.
 
 	# Constants
-	LCTN_SCR_TXT = (SIZE_SNEK, SIZE_SNEK)
-	SIZE_SCR_TXT = SIZE_WIN[0] // 40
-	SIZE_TTL_TXT = SIZE_WIN[0] // 20
+	LCTN_SCR_TXT = (SIZE_SNEK, SIZE_SNEK)	# location on which score will be displayed 
+	SIZE_SCR_TXT = SIZE_WIN[0] // 40		# score text size
+	SIZE_TTL_TXT = SIZE_WIN[0] // 20		# title text size
 
 
 ###
@@ -46,7 +46,7 @@ def displayText(disp, text_type, text, msg_location=None):
 		color = CFG.CLR_TTL_TXT
 		size = CFG.SIZE_TTL_TXT
 
-	font_title = pygame.font.SysFont(None, size)
+	font_title = font.SysFont(None, size)
 	msg = font_title.render(text, True, color)
 	if msg_location == None:
 		msg_location = msg.get_rect()
@@ -60,18 +60,18 @@ def randCoord():
 
 def drawWithBorder(disp, clr, x, y, size, bclr=None):
 	if bclr != None:
-		pygame.draw.rect(disp, bclr, [x, y, size, size], 1)		# draw entity border
-		pygame.draw.rect(disp, clr, [x+1, y+1, size-2, size-2])	# draw entity itself
+		draw.rect(disp, bclr, [x, y, size, size], 1)		# draw entity border
+		draw.rect(disp, clr, [x+1, y+1, size-2, size-2])	# draw entity itself
 	else:
-		pygame.draw.rect(disp, clr, [x, y, size, size])		# draw entity itself
+		draw.rect(disp, clr, [x, y, size, size])		# draw entity itself
 
 ###
 # Main Game Loop Function
 ###
 def gameLoop():
-	disp = pygame.display.set_mode(CFG.SIZE_WIN)
-	pygame.display.update()
-	pygame.display.set_caption("Snek")
+	disp = display.set_mode(CFG.SIZE_WIN)
+	display.update()
+	display.set_caption("Snek")
 
 	snek = []	# list of coordinate tuples (x,y).
 	snek_length = 1
@@ -85,7 +85,7 @@ def gameLoop():
 	prev_move = None	# will be reset everytime key is pressed
 	food_x, food_y = randCoord()
 
-	clock = pygame.time.Clock()
+	clock = time.Clock()
 	border_food = (0,0,0) if CFG.BRDR_FOOD else None
 	border_snek = (0,0,0) if CFG.BRDR_SNEK else None
 
@@ -95,38 +95,38 @@ def gameLoop():
 			disp.fill(CFG.CLR_BG)
 			displayText(disp, "title", "YU DED MAN! Wanna play again [R] or nah [ESC]?")
 			displayText(disp, "score", "SCORE: {}".format(snek_length-1), msg_location=CFG.LCTN_SCR_TXT)	# display score
-			pygame.display.update()
+			display.update()
 
-			for e in pygame.event.get():
+			for e in event.get():
 				#print(e)
-				if e.type == pygame.KEYDOWN:
-					if e.key == pygame.K_ESCAPE:
+				if e.type == KEYDOWN:
+					if e.key == K_ESCAPE:
 						quit_game = True
 						game_over = False	# terminate [while game_over] loop
-					if e.key == pygame.K_r:
+					if e.key == K_r:
 						gameLoop()
 
-		for e in pygame.event.get():
+		for e in event.get():
 			#print(e)
-			if e.type == pygame.QUIT:
+			if e.type == QUIT:
 				quit_game = True
-			if e.type == pygame.KEYDOWN:
-				if e.key == pygame.K_LEFT and prev_move != pygame.K_RIGHT:
+			if e.type == KEYDOWN:
+				if e.key == K_LEFT and prev_move != K_RIGHT:
 					move_x = -CFG.SIZE_SNEK
 					move_y = 0
-					prev_move = pygame.K_LEFT
-				elif e.key == pygame.K_RIGHT and prev_move != pygame.K_LEFT:
+					prev_move = K_LEFT
+				elif e.key == K_RIGHT and prev_move != K_LEFT:
 					move_x = +CFG.SIZE_SNEK
 					move_y = 0
-					prev_move = pygame.K_RIGHT
-				elif e.key == pygame.K_UP and prev_move != pygame.K_DOWN:
+					prev_move = K_RIGHT
+				elif e.key == K_UP and prev_move != K_DOWN:
 					move_y = -CFG.SIZE_SNEK
 					move_x = 0
-					prev_move = pygame.K_UP
-				elif e.key == pygame.K_DOWN and prev_move != pygame.K_UP:
+					prev_move = K_UP
+				elif e.key == K_DOWN and prev_move != K_UP:
 					move_y = +CFG.SIZE_SNEK
 					move_x = 0
-					prev_move = pygame.K_DOWN
+					prev_move = K_DOWN
 
 		if x >= CFG.SIZE_WIN[0] or x < 0 or y >= CFG.SIZE_WIN[1] or y < 0:
 			if CFG.PORTAL_WALLS:
@@ -141,8 +141,8 @@ def gameLoop():
 		disp.fill(CFG.CLR_BG)
 		# first create food, after that snek so that when snek eats the food,
 		# snek is over the food in display.
-		# pygame.draw.rect(disp, (0, 0, 0), [food_x, food_y, CFG.SIZE_SNEK, CFG.SIZE_SNEK], 2)	# draw food border
-		# pygame.draw.rect(disp, CFG.CLR_FOOD, [food_x+1, food_y+1, CFG.SIZE_SNEK-2, CFG.SIZE_SNEK-2])	# draw food
+		# draw.rect(disp, (0, 0, 0), [food_x, food_y, CFG.SIZE_SNEK, CFG.SIZE_SNEK], 2)	# draw food border
+		# draw.rect(disp, CFG.CLR_FOOD, [food_x+1, food_y+1, CFG.SIZE_SNEK-2, CFG.SIZE_SNEK-2])	# draw food
 		drawWithBorder(disp, CFG.CLR_FOOD, food_x, food_y, CFG.SIZE_SNEK, bclr=border_food)	# draw food
 		head = (x, y)
 		snek.append(head)			# first, append new part to snek
@@ -156,13 +156,13 @@ def gameLoop():
 
 		for body_part in snek:		# draw snek
 			drawWithBorder(disp, CFG.CLR_SNEK, body_part[0], body_part[1], CFG.SIZE_SNEK, bclr=border_snek)
-			# pygame.draw.rect(disp, CFG.CLR_SNEK, 
+			# draw.rect(disp, CFG.CLR_SNEK, 
 			# 	[body_part[0], body_part[1], CFG.SIZE_SNEK, CFG.SIZE_SNEK]
 			# )
 
 		if CFG.ALWYS_SHW_SCR:
 			displayText(disp, "score", "SCORE: {}".format(snek_length-1), msg_location=CFG.LCTN_SCR_TXT)	# display score
-		pygame.display.update()
+		display.update()
 
 
 		if x == food_x and y == food_y:
@@ -174,12 +174,12 @@ def gameLoop():
 
 		clock.tick(CFG.SPD)
 
-	pygame.quit()
+	quit()
 
 
 ###
 # Main Program
 ###
 if __name__ == '__main__':
-	pygame.init()
+	init()
 	gameLoop()
