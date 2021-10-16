dc541dca4f0a784fe85c3d2ce7b249cd3e9bac63
appleren-the-original <alpgen99@hotmail.com>
2021-10-16 19:18:13 +0300

    FRZ: added mirror option and now time is more accurate

--- FRZfilter/frz.py
+++ FRZfilter/frz.py
@@ -3,7 +3,7 @@ import numpy as np
 import threading
 
 import tkinter
-from tkinter import Tk, Radiobutton, IntVar, Entry, Button, Label, StringVar
+from tkinter import Tk, Radiobutton, IntVar, Entry, Button, Label, StringVar, Checkbutton
 
 import time
 from datetime import datetime
@@ -17,6 +17,7 @@ import sys
 ###
 camera = 0              # which camera to use
 orientation = False     # True: scans width (horizontal), False: scans height (vertical)
+mirror = False          # True: mirrors the video image, False: do nothing
 duration = 10           # scan duration in seconds
 filename = "{time}"     # filename to save the resulting image
 closed = True           # camera wont open after closing the GUI, if this is set to True (flag for continuing)
@@ -28,6 +29,9 @@ closed = True           # camera wont open after closing the GUI, if this is set
 def selectOrientation():
     global orientation
     orientation = orientation_var.get()
+def setMirror():
+    global mirror
+    mirror = mirror_var.get()
 
 def validate(event=None):
     global duration, filename, closed
@@ -52,6 +56,10 @@ radio2 = Radiobutton(root, text="Vertical", variable=orientation_var, value=0, c
 radio1.pack()
 radio2.pack()
 radio2.select()
+
+mirror_var = IntVar()
+mirror_checkbox = Checkbutton(root, text="Mirror Camera", variable=mirror_var, onvalue=1, offvalue=0, command=setMirror)
+mirror_checkbox.pack()
 
 duration_label = Label(text="Duration (seconds):"); duration_label.pack(pady=(10,0))
 duration_var = StringVar()
@@ -88,7 +96,8 @@ def getTimestampString():
     return "_".join(str(datetime.now()).split(" ")).replace(":", ".")[:-7]
 
 
-duration -= 1           # it lasts 1 sec longer than given value, so decrement by 1.
+duration -= 3           # it lasts ~3 sec longer than given value, so decrement by 3. 
+                        # (TODO: for more accurate result, use timer instead of sleep)
 scan = 0
 vid = cv2.VideoCapture(camera)
 vid.set(3, 1920)
@@ -107,6 +116,16 @@ cpy = res.copy()
 # duration/w    sec  -> 1            px
 
 
+# helper function. provides atomicity
+def read_frame():
+    global vid, mirror 
+    _, frame_local = vid.read()
+    if mirror:
+        frame_local = cv2.flip(frame_local, 1)
+    return frame_local
+
+
+# thread target function
 def scanner():
     global w, h, duration, frame, cpy, res, scan
     print("> Started Scanning\n")
@@ -127,8 +146,7 @@ def scanner():
             scan += 1
 
 
-
-
+# thread target function
 def displayer():
     global w, h, duration, frame, cpy, res, scan, filename
     condition = w if orientation else h
@@ -136,7 +154,7 @@ def displayer():
     start_time = time.time()
     
     while scan < condition:
-        _, frame = vid.read()
+        frame = read_frame()
         # frame = cv2.flip(frame, 1)
         cv2.imshow("final", cpy)
         k = cv2.waitKey(1) & 0xff
@@ -156,6 +174,8 @@ def displayer():
         filename = getTimestampString()
     cv2.imwrite(filename + ".jpg", res)
     print("> Image saved.\n")
+
+
 
 t1 = threading.Thread(target=displayer);
 t2 = threading.Thread(target=scanner);
